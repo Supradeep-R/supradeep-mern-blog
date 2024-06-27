0f91d4a372f58390f18701ee0f50cd09af1636a2
Supradeep-R <revuru.supradeep@gmail.com>
2024-06-27 07:32:04 +0530

    create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../src/Pages/Home";
+import About from "../src/Pages/About";
+import Dashboard from "../src/Pages/Dashboard";
+import Projects from "../src/Pages/Projects";
+import Signin from "../src/Pages/Signin";
+import Signup from "../src/Pages/Signup";
 
 const App = () => {
-  return <div className="text-3xl">App autosave </div>;
+  return (
+    <div className="text-3xl">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/sign-in" element={<Signin />} />
+        <Route exact path="/sign-up" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
