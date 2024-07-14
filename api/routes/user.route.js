69c37b7d80e1bf53498dfa5044e5d079c2f5ea93
Supradeep-R <revuru.supradeep@gmail.com>
2024-07-14 12:00:36 +0530

    update deleteroute with authmiddleware

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,21 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware')
+const authMiddleware = require("../middlewares/authMiddleware");
 
-const {createPost,viewPosts , viewSinglePost,updateSinglePost,viewAuthorPosts,deletePost}=require('../controllers/user.controller');
+const {
+  createPost,
+  viewPosts,
+  viewSinglePost,
+  updateSinglePost,
+  viewAuthorPosts,
+  deletePost,
+} = require("../controllers/user.controller");
 
-router.post('/create-post',authMiddleware,createPost)
-router.get('/view-posts',authMiddleware,viewPosts)
-router.get('/view-single-post/:id',authMiddleware,viewSinglePost)
-router.put('/update-single-post/:id',authMiddleware,updateSinglePost)
-router.get('/view-author-posts/:id',authMiddleware,viewAuthorPosts)
-router.delete('/delete-post/:id', deletePost);
+router.post("/create-post", authMiddleware, createPost);
+router.get("/view-posts", authMiddleware, viewPosts);
+router.get("/view-single-post/:id", authMiddleware, viewSinglePost);
+router.put("/update-single-post/:id", authMiddleware, updateSinglePost);
+router.get("/view-author-posts/:id", authMiddleware, viewAuthorPosts);
+router.delete("/delete-post/:id", authMiddleware, deletePost);
 
-module.exports=router;
+module.exports = router;
