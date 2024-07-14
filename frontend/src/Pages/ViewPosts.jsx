74dd5eaa66e26ec5111485625bea7afd51f115cb
Supradeep-R <revuru.supradeep@gmail.com>
2024-07-14 10:17:57 +0530

    update loading for view posts

--- frontend/src/Pages/ViewPosts.jsx
+++ frontend/src/Pages/ViewPosts.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { UserContext } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
+import { Spinner } from "flowbite-react";
 import PostCard from "../Components/PostCard";
-
 
 const ViewPosts = () => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { userInfo } = useContext(UserContext);
   const navigate = useNavigate();
 
@@ -26,6 +27,8 @@ const ViewPosts = () => {
         setPosts(response.data);
       } catch (error) {
         console.error("Error fetching posts:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -34,12 +37,18 @@ const ViewPosts = () => {
 
   return (
     <div>
-      {posts.length > 0 &&
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <Spinner size="xl" />
+        </div>
+      ) : (
+        posts.length > 0 &&
         posts.map((post) => (
           <div key={post._id} className="p-4">
             <PostCard {...post} />
           </div>
-        ))}
+        ))
+      )}
     </div>
   );
 };
